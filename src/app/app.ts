b00403e2d365a56a04acbd318bc2529bb323b211
Henry Kanemoto <kanemotohenry@gmail.com>
2026-07-06 17:23:39 -0300

    chore: remove importação do Router

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+//!import { RouterOutlet } from '@angular/router'; // Remove importação do Router
+import { Produto } from './components/produto/produto';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [Produto],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
