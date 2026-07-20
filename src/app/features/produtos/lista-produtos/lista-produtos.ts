810d0db3adc8b9e4deff56b2f8e762c3face417e
HenryKanemoto <kanemotohenry@gmail.com>
2026-07-20 14:30:34 -0300

    chore (lista-produtos): Adiciona função de carrinho e adicionar produtos ao carrinho

--- src/app/features/produtos/lista-produtos/lista-produtos.ts
+++ src/app/features/produtos/lista-produtos/lista-produtos.ts
@@ -1,4 +1,4 @@
-import { Component, signal, computed } from '@angular/core';
+import { Component, signal, computed, effect } from '@angular/core';
 import { Produto } from '../produto/produto';
 import { PrecoFormatadoPipe } from '../../../shared/pipes/preco-formatado-pipe';
 
@@ -27,22 +27,59 @@ export class ListaProdutos {
       preco: -2899.99
     }
   ]);
-  exibirProduto(nome: string) {
-    console.log('Produto Selecionado: ', nome)
-  }
-  adicionarProduto(nome: string, preco: number) {
-    this.produtos.update(listaAtual => [
-      ...listaAtual, {nome: nome, preco: preco}
-    ]);
-  }
-  totalProdutos = computed(() => this.produtos().length);
 
-  valorTotal = computed(() => {return this.produtos().reduce(
-    (total, item) => total + item.preco, 0)});
+  produtoSelecionado = signal<string | null>(null);
+  totalProdutos = computed(() => this.produtos().length);
   
-  subtituirProdutos () {
-    this.produtos.set([
-      {nome: 'Arroz', preco: 100}
-    ]);
+  exibirProduto(nome: string) {
+    console.log('Produto Selecionado: ', nome);
+  };
+  
+  valorTotal = computed(() => {
+    return this.produtos().reduce(
+    (total, item) => total + item.preco, 0)
+  });
+  
+    adicionarProduto() {
+      this.produtos.update(listaAtual => [
+        ...listaAtual, 
+        {nome: 'Teclado', preco: 250}
+      ]);
+    }
+    subtituirProdutos () {
+      this.produtos.set([
+        {nome: 'Arroz', preco: 100}
+      ]);
+    }
+  
+  constructor() {
+    effect(() => {
+      console.log('Lista de produtos alterada: ', this.produtos());
+    });
+    effect(() => {
+      console.log('Valor total atualizado: ', this.valorTotal);
+    });
+    effect(() => {
+      if(typeof document !== 'undefined') {
+        document.title = `(${this.totalProdutos()}) Minha Loja`;
+      }
+    });
+  }
+
+  carrinho = signal<{nome: string; preco: number}[]>([]);
+  quantidade_carrinho = computed( () => this.carrinho().length);
+  total_carrinho = computed(() => {
+    return this.carrinho().reduce(
+      (total, item) => total + item.preco, 0
+    )
+  });
+  adicionarAoCarrinho (produto: {nome: string; preco: number}) {
+    this.carrinho.update(listaAtual => [
+      ...listaAtual,
+      produto
+    ])
+  }
+  removeTodosProdutos() {
+    this.produtos.set([]);
   }
 }
