beb689236f3907cf0c2aaeb7aeef5dec63d884ab
HenryKanemoto <kanemotohenry@gmail.com>
2026-07-20 17:09:36 -0300

    feat (app): Adiciona nav para navegar entre páginas do router

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,13 +1,14 @@
 import { Component, signal } from '@angular/core';
-import { ListaProdutos } from './features/produtos/lista-produtos/lista-produtos';
-//!import { RouterOutlet } from '@angular/router'; // Remove importação do Router
+// import { ListaProdutos } from './features/produtos/lista-produtos/lista-produtos';
+import { RouterOutlet, RouterLink } from '@angular/router'; 
 
 @Component({
   selector: 'app-root',
-  imports: [ListaProdutos],
+  imports: [RouterOutlet, RouterLink],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   protected readonly title = signal('e-commerce');
+  nomeLoja = "NDA-Mt'Ofensiv"; // nome do e-commerce
 }
